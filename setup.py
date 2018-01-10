1c4249016d188f4d6330e761aac8bb43ef5a3d7b
James Huber <huberj001@gmail.com>
2018-01-09 23:01:08 -0500

    update to fix errors returned from pip install with python3.6+

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-import uuid
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from codecs import open
+from os import path
 
-install_reqs=parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pyfmg',
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     url='https://github.com/p4r4n0y1ng/pyfmg',
     license='Apache 2.0',
@@ -15,5 +17,6 @@ setup(
     author_email='jhuber@fortinet.com',
     description='Represents the base components of the Fortinet FortiManager JSON-RPC interface',
     include_package_data=True,
-    install_requires=reqs
+    long_description=long_description,
+    install_requires=['requests']
 )
