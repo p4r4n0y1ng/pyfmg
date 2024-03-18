a26eeef15857bdaa56fc5f7f626a7b3f28c4669e
James Huber <huberj001@gmail.com>
2024-03-18 15:51:50 -0400

    updating setup.py for install use

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyfmg',
-    version='0.8.6.0',
+    version='0.8.6.1',
     packages=find_packages(),
     url='https://github.com/p4r4n0y1ng/pyfmg',
     license='Apache 2.0',
